f7c38917b09dad096a3da5a5a8b1bbe3f6a58248
Vitali Avagyan <eheva87@gmail.com>
2020-02-14 22:02:43 +0000

    refactor to promise

--- database.js
+++ database.js
@@ -1,7 +1,10 @@
 var data = 'data/tools.json';
 
 function getAll{
-    print('Hello World');
-    print('what is happening?')
+    print('Hello dude!');
+    print('what is happening?');
+    for(i = 1, i < 5, i++) {
+        i;
+    }
 }
 var promise = require('promise');
